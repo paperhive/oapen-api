46b338a675d0f93a74821ab52e7cb237305783cf
Jenny Gebske <jennifer@gebske.de>
2016-04-14 15:36:35 +0200

    comment out db

--- server.js
+++ server.js
@@ -3,27 +3,27 @@ var router = require('koa-router')();
 var pg = require('pg');
 pg.defaults.ssl = true;
 
-var db;
-
-pg.connect(process.env.DATABASE_URL, function(error, client) {
-  if(error) throw error;
-
-  // client
-  //   .query('SELECT table_schema,table_name FROM information_schema.tables;')
-  //   .on('row', function(row) {
-  //     console.log(JSON.stringify(row));
-  //   });
-
-  db = database;
-  console.log('Database connection ready');
-
-  var server = app.listen(process.env.PORT || 3000, function() {
-    var port = server.address().port;
-    console.log('App now running on port', port);
-  });
-
-});
-
+// var db;
+//
+// pg.connect(process.env.DATABASE_URL, function(error, client) {
+//   if(error) throw error;
+//
+//   // client
+//   //   .query('SELECT table_schema,table_name FROM information_schema.tables;')
+//   //   .on('row', function(row) {
+//   //     console.log(JSON.stringify(row));
+//   //   });
+//
+//   db = database;
+//   console.log('Database connection ready');
+//
+//   var server = app.listen(process.env.PORT || 3000, function() {
+//     var port = server.address().port;
+//     console.log('App now running on port', port);
+//   });
+//
+// });
+//
 
 router
   .get('/', function *(next) {
@@ -34,4 +34,4 @@ app
   .use(router.routes())
   .use(router.allowedMethods());
 
-
+app.listen(process.env.PORT || 3000);
