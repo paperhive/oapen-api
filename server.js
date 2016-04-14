825826b0da381ca72865e58cb6aaadfdd3190bab
Jenny Gebske <jennifer@gebske.de>
2016-04-14 12:32:28 +0200

    add port

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var app = require('koa')();
 var router = require('koa-router')();
 
+console.log('starting...');
+
 router
   .get('/', function *(next) {
     this.body = 'Hello World';
@@ -10,5 +12,5 @@ app
   .use(router.routes())
   .use(router.allowedMethods());
 
-// app.listen(3000);
-// console.log('listening on port 3000');
+app.listen(process.env.PORT || 3000);
+console.log('started!');
