4099bd93fccd497a226728696f300b8901ac399a
Jenny Gebske <jennifer@gebske.de>
2016-04-14 15:42:00 +0200

    add db

--- server.js
+++ server.js
@@ -3,27 +3,16 @@ var router = require('koa-router')();
 var pg = require('pg');
 pg.defaults.ssl = true;
 
-// var db;
-//
-// pg.connect(process.env.DATABASE_URL, function(error, client) {
-//   if(error) throw error;
-//
-//   // client
-//   //   .query('SELECT table_schema,table_name FROM information_schema.tables;')
-//   //   .on('row', function(row) {
-//   //     console.log(JSON.stringify(row));
-//   //   });
-//
-//   db = database;
-//   console.log('Database connection ready');
-//
-//   var server = app.listen(process.env.PORT || 3000, function() {
-//     var port = server.address().port;
-//     console.log('App now running on port', port);
-//   });
-//
-// });
-//
+pg.connect(process.env.DATABASE_URL, function(error, client) {
+  if(error) throw error;
+
+  console.log('Database connection ready');
+
+  var server = app.listen(process.env.PORT || 3000, function() {
+    var port = server.address().port;
+    console.log('App now running on port', port);
+  });
+});
 
 router
   .get('/', function *(next) {
@@ -33,5 +22,3 @@ router
 app
   .use(router.routes())
   .use(router.allowedMethods());
-
-app.listen(process.env.PORT || 3000);
