56756129d26b106d735daad4927de797c04aa5e3
Jenny Gebske <jennifer@gebske.de>
2016-04-21 12:38:49 +0200

    add check if db exists

--- database.js
+++ database.js
@@ -9,10 +9,13 @@ pg.defaults.ssl = !(process.env.DATABASE_SSL === 'false');
 exports.init = co.wrap(function *init() {
   const connect = bluebird.promisify(pg.connect, {context: pg});
   const client = yield connect(process.env.DATABASE_URL);
+  const query = bluebird.promisify(client.query, {context: client});
   console.log('Database connection ready');
+
+  yield query('CREATE TABLE IF NOT EXISTS oapen_data (id integer, timestamp timestamp without time zone, data jsonb);');
 
   return {
     client,
-    query: bluebird.promisify(client.query, {context: client}),
+    query,
   };
 });
